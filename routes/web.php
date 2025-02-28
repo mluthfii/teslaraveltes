069d1b001b033cd60b81631b65226481583c185f
mluthfii <muhluthfii18@gmail.com>
2025-02-28 20:49:33 +0700

    Frontend

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About Us']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact Us!']);
+});
+
+Route::get('/calc', function () {
+    return view('calc', ['title'=> 'Lets Calculate!']);
+});
